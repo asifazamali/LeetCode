8040e6435957ea1202cb00894f20f10f2e969c1e
asifazamali <asifazamali@gmail.com>
2022-04-08 08:17:11 +0530

    added gitIgnore

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import KMPSearch.KMPSerch;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+//        KMPSerch kmp = new KMPSerch();
+//        kmp.search("AAB", "AABAACAADAABAABA");
+    }
+}
